1b6d1b6ef89031393ec3ef45e8e3cd6d5c458183
EverlastingLilflo <anjorin199@gmail.com>
2023-05-26 00:54:16 +0100

    installed firebase in my project folder

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {collection, getDocs, getFireStore, snapshotEqual} from "firebase/firestore"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC-vvsMkOi8B5Wg4VgkJouaQuszIfEX-ks",
+//   authDomain: "first-learn-293b9.firebaseapp.com",
+//   projectId: "first-learn-293b9",
+//   storageBucket: "first-learn-293b9.appspot.com",
+//   messagingSenderId: "1000788914787",
+//   appId: "1:1000788914787:web:048b76c88472ff42dd7e56"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+// const efe = getFireStore()
+
+// const efre = collection(efe, 'users')
+
+// getDocs(efre)
+// .then((snapshot)=>){
+//   console.log(snapshot);
+// }
